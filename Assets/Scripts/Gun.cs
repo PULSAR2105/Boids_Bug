0370cc2db718b316abaaf2b6cfbd9b48050e34a3
PULSAR2105 <54115653+PULSAR2105@users.noreply.github.com>
2021-11-26 19:24:10 +0100

    Gun AI + New sprit

--- Assets/Scripts/Gun.cs
+++ Assets/Scripts/Gun.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Gun : MonoBehaviour
-{    
+{
     public float distanceHit;
     public LineRenderer ray;
     Transform[] Positions;
@@ -16,24 +16,60 @@ public class Gun : MonoBehaviour
     RaycastHit2D hitFriend;
     RaycastHit2D hitWall;
 
-    void Start()
-    {
+    GameObject[] friends;
+    GameObject goal;
+    GameObject target;
+    public float speedRotation;
+    float angle;
+    Vector3 vectorToTarget;
+    Quaternion q;
+    float mindist;
+    float dist;
+
+    void Start() {
         ray.useWorldSpace = true;
         ray.positionCount = 2;
         ray.SetPosition(0, transform.position);
         ray.SetPosition(1, transform.position);
+
+        goal = GameObject.Find("Goal");
     }
 
     void FixedUpdate() {
-       hitFriend = Physics2D.Raycast(transform.position, transform.right, distanceHit, 4);
-       hitWall = Physics2D.Raycast(transform.position + transform.right * 1.5f, transform.right, distanceHit);
-       
-       if(hitFriend && hitWall) {
-            StartCoroutine(shot());
-       } 
-       if(hitFriend && !hitWall) {
-            StartCoroutine(shot());
-       }
+        mindist = Mathf.Infinity;
+        friends = GameObject.FindGameObjectsWithTag("Boid");
+
+        foreach (GameObject friend in friends) {
+            dist = Vector3.Distance(friend.transform.position, transform.position);
+            if (dist < mindist)
+            {
+                mindist = dist;
+                target = friend;
+            }
+            
+        }
+
+        dist = Vector3.Distance(goal.transform.position, transform.position);
+        if (dist < mindist)
+        {
+            mindist = dist;
+            target = goal;
+        }
+
+        vectorToTarget = target.transform.localPosition - transform.localPosition;
+        angle = Mathf.Atan2(vectorToTarget.y, vectorToTarget.x) * Mathf.Rad2Deg;
+        q = Quaternion.AngleAxis(angle, Vector3.forward);
+        transform.rotation = Quaternion.Slerp(transform.rotation, q, speedRotation * Time.deltaTime);
+
+        hitFriend = Physics2D.Raycast(transform.position, transform.right, distanceHit, 4);
+        hitWall = Physics2D.Raycast(transform.position + transform.right * 1.5f, transform.right, distanceHit);
+        
+        if(hitFriend && hitWall) {
+             StartCoroutine(shot());
+        } 
+        if(hitFriend && !hitWall) {
+             StartCoroutine(shot());
+        }
     }
 
     IEnumerator shot() {
